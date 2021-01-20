946229b784cd9c8674a57e81115c95aaff6bcbfa
Bilal AMRI <iambilalamri@gmail.com>
2021-01-20 19:35:36 +0100

    [master]: working on condition in Python

--- control-flow.py
+++ control-flow.py
@@ -1,2 +1,18 @@
 # CONTROL FLOW
 #######################################
+
+print(10 == "10")  # False
+print(10 != "10")  # True
+print("bag" > "apple")  # True
+print("bag" == "BAG")  # False
+print(ord('b'))  # 98
+print(ord('a'))  # 97
+print(ord('B'))  # 66
+
+########################################
+
+temperature = 37
+if temperature > 30:
+    print("It's warm")
+    print("Drink Water")
+print("Done")
