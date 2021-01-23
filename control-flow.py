9e3a02fee38073c5108c14ed9c3986b9cadf0939
Bilal AMRI <iambilalamri@gmail.com>
2021-01-23 20:54:34 +0100

    [master]- working with loops in python & add function's file

--- control-flow.py
+++ control-flow.py
@@ -61,9 +61,62 @@ for number in range(3):
     print("Attempt", number + 1, (number + 1) * ".")
 
 # precise the range
+
 for number in range(1, 4):
     print("Attempt", number, (number) * ".")
 
 # increment by 2 in loop
+
 for number in range(1, 10, 2):
     print("Attempt", number, (number) * ".")
+
+# if else
+
+successfull = False
+for number in range(3):
+    print("Attempt")
+    if successfull:
+        print("Successfull")
+        break
+else:
+    print("Attempt 3 times and failed")
+
+# Nested loop
+
+for x in range(5):  # outer loop
+    for y in range(3):  # inner loop
+        print(f"({x}, {y})")
+
+print(type(5))  # <class 'int'>
+print(type(range(5)))  # <class 'range'>
+
+# range and string and list are iterable
+# ex: for i in "Python":
+
+# While Loop1
+
+number = 100
+while number > 0:
+    print(number)
+    number //= 2
+
+# While Loop2
+# command1 = ""
+# while command1.lower() != "quit":
+#     command1 = input(">")
+#     print("ECHO", command1)
+
+# While Loop3
+while True:
+    command2 = input(">")
+    print("ECHO", command2)
+    if command2.lower() == "quit":
+        break
+
+# Exercice
+count = 0
+for number in range(1, 10):
+    if number % 2 == 0:
+        count += 1
+        print(number)
+print(f"We have {count} even numbers")
