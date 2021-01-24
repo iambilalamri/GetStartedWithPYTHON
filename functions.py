0c766511012ea7137a7cb2d5c7176db29f2500a4
Bilal AMRI <iambilalamri@gmail.com>
2021-01-24 05:08:10 +0100

    [master]- working with function in Python

--- functions.py
+++ functions.py
@@ -55,3 +55,20 @@ def save_user(**user):
 
 # {'id': 1, 'name': 'John', 'age': 22} is object called dictionnary
 save_user(id=1, name="John", age=22)
+
+
+# Exercice
+print("#####################################")
+
+
+def fizz_buzz(input):
+    if (input % 3 == 0) and (input % 5 == 0):
+        return "FizzBuzz"
+    if input % 3 == 0:
+        return "Fizz"
+    if input % 5 == 0:
+        return "Buzz"
+    return input
+
+
+print(fizz_buzz(15))
