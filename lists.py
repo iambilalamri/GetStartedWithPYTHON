1f16af30e18e80fe8491d5e03e2a897f82345c65
Bilal AMRI <iambilalamri@gmail.com>
2021-01-25 21:14:41 +0100

    [master]- working with list in Python

---
+++ lists.py
@@ -0,0 +1,9 @@
+print("LISTS")
+print("##########################################")
+letters = ["a", "b", "c"]
+matrix = [[0, 1], [2, 3], [4, 5]]
+zeros = [0] * 5  # array with 5 zeros
+combined = letters + zeros  # combined string and number in same array
+numbers = list(range(20))  # get a list of numbers of 0 to 19
+chars = list("Hello World")  # get list of chars
+print(numbers)
